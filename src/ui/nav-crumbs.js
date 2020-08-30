06e41dbdae4a41e1fa9cf15fced72724eb61195c
chrisrzhou <chrisrzhou@pm.me>
2020-08-30 01:04:18 -0700

    docs: update compiler and parser recipes

--- src/ui/nav-crumbs.js
+++ src/ui/nav-crumbs.js
@@ -19,20 +19,22 @@ export default function NavCrumbs() {
       return [...acc, { label, route }];
     }, []);
 
-  return crumbs.length > 0 && (
-    <h2>
-      {crumbs.map((crumb, i) => {
-        const { label, route } = crumb;
-        const Content =
-          i < crumbs.length - 1 ? (
-            <>
-              <Link to={route}>{label}</Link>/
-            </>
-          ) : (
-            label
-          );
-        return <React.Fragment key={route}>{Content}</React.Fragment>;
-      })}
-    </h2>
+  return (
+    crumbs.length > 0 && (
+      <h2>
+        {crumbs.map((crumb, i) => {
+          const { label, route } = crumb;
+          const Content =
+            i < crumbs.length - 1 ? (
+              <>
+                <Link to={route}>{label}</Link>/
+              </>
+            ) : (
+              label
+            );
+          return <React.Fragment key={route}>{Content}</React.Fragment>;
+        })}
+      </h2>
+    )
   );
 }
