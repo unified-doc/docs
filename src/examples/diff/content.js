0c35ae4dec2772b94e0b5062d4b5a7b2c09fdbcb
chrisrzhou <chrisrzhou@pm.me>
2020-08-30 15:27:10 -0700

    docs(examples-diff): add `DiffExample`

---
+++ src/examples/diff/content.js
@@ -0,0 +1,49 @@
+export const htmlContent = `
+<h1>Lorem Ipsum Presents</h1>
+
+<pre>
+var name = "World";
+console.warn("Hello, " + name + "!");
+</pre>
+
+<p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+
+<h2>Header Level 2</h2>
+
+<ol>
+   <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+   <li>Aliquam tincidunt mauris eu risus.</li>
+</ol>
+
+<blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p></blockquote>
+
+<h3>Header Level 3</h3>
+
+<ul>
+   <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+   <li>Aliquam tincidunt mauris eu risus.</li>
+</ul>
+`;
+
+export const markdownContent = `
+# Lorem Ipsum Presents
+
+${'```'}js
+var name = "World";
+console.warn("Hello, " + name + "!");
+${'```'}
+
+**Pellentesque habitant morbi tristique** senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. _Aenean ultricies mi vitae est._ Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, ${'`'}commodo vitae${'`'}, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. [Donec non enim](#) in turpis pulvinar facilisis. Ut felis.
+
+## Header Level 2
+
+1. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+2. Aliquam tincidunt mauris eu risus.
+
+> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.
+
+### Header Level 3
+
+- Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+- Aliquam tincidunt mauris eu risus.
+`;
---
+++ src/examples/diff/index.js
@@ -0,0 +1,93 @@
+import { diffChars } from 'diff';
+import React, { createElement, useState } from 'react';
+import rehype2react from 'rehype-react';
+import unifiedDoc from 'unified-doc';
+import { v4 as uuidv4 } from 'uuid';
+
+import { Box, Card, Flex, Textarea } from '~/ui';
+
+import { markdownContent, htmlContent } from './content';
+
+function diffContents(content1, content2) {
+  const marks = [];
+  let content = '';
+  let start = 0;
+  let end = 0;
+
+  const diff = diffChars(content1, content2);
+
+  diff.forEach((part) => {
+    const { added, count, removed, value } = part;
+    content += value;
+    end += count;
+    if (added || removed) {
+      marks.push({
+        classNames: [added ? 'diff-added' : 'diff-removed'],
+        id: uuidv4(),
+        start,
+        end,
+        value,
+      });
+    }
+    start = end;
+  });
+
+  return { marks, content };
+}
+
+export default function DiffDemo() {
+  const [content1, setContent1] = useState(markdownContent);
+  const [content2, setContent2] = useState(htmlContent);
+
+  // apply diff algorithms
+  const { marks, content } = diffContents(content1, content2);
+
+  // construct the diffed doc
+  const doc = unifiedDoc({
+    compiler: [[rehype2react, { createElement }]],
+    content,
+    filename: 'diff.txt',
+    sanitizeSchema: {
+      attributes: {
+        '*': ['className', 'style'],
+        mark: ['dataMarkId'],
+      },
+    },
+    marks,
+  });
+
+  const diffStyles = {
+    '.diff-added': {
+      background: '#acf2bd',
+      color: 'black',
+    },
+    '.diff-removed': {
+      background: '#fdb8c0',
+      color: 'black',
+    },
+  };
+
+  return (
+    <Flex flexDirection="column" space={3}>
+      <Flex space={3} sx={{ maxHeight: 400 }}>
+        <Textarea
+          flex="1 1 auto"
+          label="Content 1"
+          id="content-1"
+          value={content1}
+          onChange={setContent1}
+        />
+        <Textarea
+          flex="1 1 auto"
+          label="Content 2"
+          id="content-2"
+          value={content2}
+          onChange={setContent2}
+        />
+      </Flex>
+      <Box sx={diffStyles}>
+        <Card variant="doc">{doc.compile().result}</Card>
+      </Box>
+    </Flex>
+  );
+}
